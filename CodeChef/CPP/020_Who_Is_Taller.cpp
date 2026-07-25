9dabe807769d94026dc647f04ad7cdb7eef849aa
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-07-25 19:45:41 +0530

    Solved Who Is Taller

---
+++ CodeChef/CPP/020_Who_Is_Taller.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X > Y)
+            cout << "A" << endl;
+        else
+            cout << "B" << endl;
+    }
+
+    return 0;
+}
