affcdb879ff9f2dcde323aa898c96b230ce7003a
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-07-25 19:42:37 +0530

    Solved Bone Appetit

---
+++ CodeChef/CPP/019_Bone_Appetit.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M, X, Y;
+    cin >> N >> M;
+    cin >> X >> Y;
+
+    cout << (N * X) + (M * Y) << endl;
+
+    return 0;
+}
