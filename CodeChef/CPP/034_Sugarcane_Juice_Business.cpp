48b3c0513add677fd74b5bbe0e199a120d49e7de
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-07-30 20:28:49 +0530

    Solved Sugarcane Juice Business

---
+++ CodeChef/CPP/034_Sugarcane_Juice_Business.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int income = 50 * N;
+        cout << income * 30 / 100 << endl;
+    }
+
+    return 0;
+}
