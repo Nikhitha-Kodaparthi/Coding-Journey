99bd2149b142411d2e65c4f5cc236005a635baa2
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-07-31 20:13:17 +0530

    Solved Count the Notebooks

---
+++ CodeChef/CPP/035_Count_the_Notebooks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << N * 10 << endl;
+    }
+
+    return 0;
+}
