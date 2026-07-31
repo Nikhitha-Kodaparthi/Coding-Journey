a2ee5660d6438175401d71e52e804adfdcc124ee
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-07-31 20:21:59 +0530

    Solved Car or Bike

---
+++ CodeChef/CPP/037_Car_or_Bike.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X < Y)
+            cout << "BIKE" << endl;
+        else if (Y < X)
+            cout << "CAR" << endl;
+        else
+            cout << "SAME" << endl;
+    }
+
+    return 0;
+}
