de5b300001d5753da3dcdaeed3d884ec0c254fba
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-03 20:16:12 +0530

    Solved Minimum Cars Required

---
+++ CodeChef/CPP/049_Minimum_Cars_Required.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << (N + 3) / 4 << endl;
+    }
+
+    return 0;
+}
