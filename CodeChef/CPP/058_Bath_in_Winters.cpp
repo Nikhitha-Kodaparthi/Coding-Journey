f14a5e5f486cff871a61a26c3ef0511f61b6c859
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-06 21:01:17 +0530

    Solved Bath in Winters

---
+++ CodeChef/CPP/058_Bath_in_Winters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << X / (2 * Y) << endl;
+    }
+
+    return 0;
+}
