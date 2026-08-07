af1460158ad11931049ee6d90970f39a5ff0b4a4
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-07 20:49:38 +0530

    Solved Mario and Transformation

---
+++ CodeChef/CPP/061_Mario_and_Transformation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        int state = X % 3;
+
+        if (state == 0)
+            cout << "NORMAL" << endl;
+        else if (state == 1)
+            cout << "HUGE" << endl;
+        else
+            cout << "SMALL" << endl;
+    }
+
+    return 0;
+}
