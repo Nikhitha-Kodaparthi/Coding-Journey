e2984d73611d1ce452968e9509d49bf1d427aa2c
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-08 19:24:52 +0530

    Solved Finding Square Roots

---
+++ CodeChef/CPP/067_Finding_Square_Roots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << (int)sqrt(N) << endl;
+    }
+
+    return 0;
+}
