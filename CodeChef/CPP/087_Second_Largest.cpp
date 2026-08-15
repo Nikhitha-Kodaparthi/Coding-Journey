27ec9a90cf143718c9eb71557112b625944ef3eb
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-15 20:26:50 +0530

    Solved Second Largest

---
+++ CodeChef/CPP/087_Second_Largest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        int arr[3] = {A, B, C};
+        sort(arr, arr + 3);
+
+        cout << arr[1] << '\n';
+    }
+
+    return 0;
+}
