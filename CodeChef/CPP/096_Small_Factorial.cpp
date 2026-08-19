7412c6011b66c0f0fa44d1232e6b39fa1b5fe80b
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-19 21:00:29 +0530

    Solved Small Factorial

---
+++ CodeChef/CPP/096_Small_Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        long long fact = 1;
+
+        for (int i = 1; i <= N; i++) {
+            fact *= i;
+        }
+
+        cout << fact << '\n';
+    }
+
+    return 0;
+}
