884c34244d1b93b82be48121f9bb6489ad4e0503
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-08-29 20:20:17 +0530

    Solved Best of Two

---
+++ CodeChef/CPP/113_Best_of_Two.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << max(X, Y) << '\n';
+    }
+
+    return 0;
+}
