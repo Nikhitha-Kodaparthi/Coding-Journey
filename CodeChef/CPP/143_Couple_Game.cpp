864aafe396ebeebea9ae506afcf370cf755a4633
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-07 20:10:07 +0530

    Solved Couple Game

---
+++ CodeChef/CPP/143_Couple_Game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int G, B;
+        cin >> G >> B;
+
+        cout << B - G << '\n';
+    }
+
+    return 0;
+}
