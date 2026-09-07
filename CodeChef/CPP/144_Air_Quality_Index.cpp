b99acc1aa0347255efb19df7557699f39fc04661
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-07 20:13:13 +0530

    Solved Air Quality Index

---
+++ CodeChef/CPP/144_Air_Quality_Index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X < 100)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
