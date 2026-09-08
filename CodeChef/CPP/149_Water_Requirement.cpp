4f583b59faaec8819607f6d34ef3df4f5d331f9b
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-08 18:54:39 +0530

    Solved Water Requirement

---
+++ CodeChef/CPP/149_Water_Requirement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << 2 * N << '\n';
+    }
+
+    return 0;
+}
