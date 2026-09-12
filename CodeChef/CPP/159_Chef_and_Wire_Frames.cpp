f4333720a1f971df8f9d33411ac7b1aa1ee8542b
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-12 18:19:52 +0530

    Solved Chef and Wire Frames

---
+++ CodeChef/CPP/159_Chef_and_Wire_Frames.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M, X;
+        cin >> N >> M >> X;
+
+        cout << 2 * (N + M) * X << '\n';
+    }
+
+    return 0;
+}
