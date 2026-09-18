7907619a812622c5777800b89ac923de67f92f38
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-18 18:52:40 +0530

    Solved The Cheaper Cab

---
+++ CodeChef/CPP/166_The_Cheaper_Cab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X < Y)
+            cout << "FIRST\n";
+        else if (X > Y)
+            cout << "SECOND\n";
+        else
+            cout << "ANY\n";
+    }
+
+    return 0;
+}
