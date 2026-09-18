7bbf8d02f7cd04f1ba9d18827c5961cfd53c033d
Nikhitha <kodaparthinikhitha4@gmail.com>
2026-09-18 18:55:06 +0530

    Solved Discount

---
+++ CodeChef/CPP/167_Discount.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int x;
+        cin >> x;
+
+        cout << 100 - x << '\n';
+    }
+
+    return 0;
+}
